007c58819f32e7dd42253a52b8dc4dfda890402c
change <xjx903697869@163.com>
2020-01-17 01:01:04 +0800

    动态获取图片-从后台渲染轮播图数据

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,14 @@
     <el-carousel height="700px" class="carousel">
       <el-carousel-item v-for="(item, index) in banners" :key="index">
         <!-- 动态绑定-   图片路径为nuxt.config.js中的axios     no-repeat清除图片外边距-->
-        <div class="banner" :style="`background:url(${item}) center center no-repeat;`"></div>
+        <!-- <div class="banner" :style="`background:url(${item}) center center no-repeat;`"></div> -->
+        <!-- 动态渲染图片拿到后台数据 -->
+        <div
+          class="banner"
+          :style="`
+             background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;           
+             `"
+        ></div>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -15,11 +22,22 @@ export default {
   data() {
     return {
       banners: [
-        // 图片
-        "http://157.122.54.189:9095/assets/images/th01.jfif",
-        "http://157.122.54.189:9095/assets/images/th02.jfif"
+        // 静态--图片
+        // 动态-轮播图数据
+        // "http://157.122.54.189:9095/assets/images/th01.jfif",
+        // "http://157.122.54.189:9095/assets/images/th02.jfif"
       ]
     };
+  },
+  mounted() {
+    // $axios是挂载个实例下的axios方法
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      const { data } = res.data;
+      // 服务器的轮播图数据
+      this.banners = data;
+    });
   }
 };
 </script>
