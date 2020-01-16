78f26d67ff74a60af41bab6d07df58229862d15f
change <xjx903697869@163.com>
2020-01-17 00:43:50 +0800

    03首页轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <!-- 轮播图 -->
+    <el-carousel height="700px" class="carousel">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <!-- 动态绑定-   图片路径为nuxt.config.js中的axios     no-repeat清除图片外边距-->
+        <div class="banner" :style="`background:url(${item}) center center no-repeat;`"></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      banners: [
+        // 图片
+        "http://157.122.54.189:9095/assets/images/th01.jfif",
+        "http://157.122.54.189:9095/assets/images/th02.jfif"
+      ]
+    };
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.carousel {
+  min-width: 1000px;
+}
+.banner {
+  height: 700px;
+}
 </style>
